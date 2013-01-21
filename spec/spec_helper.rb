1d14332b3cce24f7fe7a7765939d0c004c9a4328
Markus Schirp <mbj@seonic.net>
2013-01-21 12:49:13 +0100

    Do 1.8 specific setup for rcov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ require 'rspec'
 # require spec support files and shared behavior
 Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require(f) }
 
+if RUBY_VERSION < '1.9'
+  require 'rspec/autorun'
+end
 
 RSpec.configure do |config|
 end
