72ad887faa1548be8f0c5df4ce788f0dbdb49027
Markus Schirp <mbj@seonic.net>
2013-03-01 21:06:01 +0100

    Use Devtools.init_spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,8 @@
 # encoding: utf-8
 
 require 'anima'
-require 'rspec'
-
-# require spec support files and shared behavior
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require(f) }
-
-if RUBY_VERSION < '1.9'
-  require 'rspec/autorun'
-end
+require 'devtools'
+Devtools.init_spec_helper
 
 RSpec.configure do |config|
 end
