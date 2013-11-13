b0953c8d01285303695588ca85abbfd79190a1ab
snusnu <gamsnjaga@gmail.com>
2013-11-13 21:18:08 +0100

    Fix specs for Anima::Error

--- spec/unit/anima/error_spec.rb
+++ spec/unit/anima/error_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Anima::Error, '#message' do
-  let(:object) { described_class.new(klass, name) }
+  let(:object) { error.new(klass, name) }
+
+  let(:error) do
+    Class.new(described_class) do
+      def self.name
+        'TestError'
+      end
+    end
+  end
 
   subject { object.message }
 
@@ -9,7 +17,7 @@ describe Anima::Error, '#message' do
   let(:name)  { 'THE-ATTRIBUTE-NAME' }
 
   it 'should return the message string' do
-    should eql('Error attribute(s) "THE-ATTRIBUTE-NAME" for THE-CLASS-NAME')
+    should eql('TestError attribute(s) "THE-ATTRIBUTE-NAME" for THE-CLASS-NAME')
   end
 
   it_should_behave_like 'an idempotent method'
